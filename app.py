624e4cf5456d9e411a9266a4d6d2abc3db2d4694
GurraT <gustafthomsson@hotmail.com>
2021-03-27 20:30:33 +0000

    Sat routing and activated links within base.html in app.py

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 import os
-from flask import (Flask)
+from flask import (Flask,render_template)
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return render_template("base.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/omx")
+def omx():
+    return render_template("omx.html")
+
+
+@app.route("/stockmarket")
+def stockmarket():
+    return render_template("stockmarket.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/toolbox")
+def toolbox():
+    return render_template("toolbox.html")
+
 
 if __name__=="__main__":
     app.run(
