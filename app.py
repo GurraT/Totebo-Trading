6c4464cdb1dd15f4ee958d8c3c521a2b5f640d48
GurraT <gustafthomsson@hotmail.com>
2021-03-28 15:02:23 +0000

    Setting up environment, next step connected to Heroku

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from flask import (Flask,render_template)
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
@@ -35,6 +37,6 @@ def toolbox():
 
 if __name__=="__main__":
     app.run(
-        os.environ.get("IP","0.0.0.0"),
-        port=int(os.environ.get("PORT","5000")),
+        os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=True)
