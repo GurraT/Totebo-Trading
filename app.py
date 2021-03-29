8c72b26e838f5a53d60948b2d96fc0eb44555a2d
GurraT <gustafthomsson@hotmail.com>
2021-03-29 17:29:33 +0000

    Push code to Heroku, installed pymongo + dnspython, and tried connection to mongoDB

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import os
-from flask import (Flask,render_template)
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def index():
-    return render_template("base.html")
+@app.route("/get_stockinfo")
+def get_stockinfo():
+    stockinfo=list(mongo.db.tasks.find())
+    return render_template("stockmarket.html",tasks=stockinfo)
 
 
 @app.route("/login")
