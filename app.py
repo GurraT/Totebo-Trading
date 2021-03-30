186f3327ff4cf8e5ed1f2d0887cedfcd98767ad3
GurraT <gustafthomsson@hotmail.com>
2021-03-30 22:21:55 +0000

    Fixed error posting to MongoDB, faulty in html-file methods instead of method

--- app.py
+++ app.py
@@ -61,11 +61,6 @@ def omx():
     return render_template("omx.html")
 
 
-@app.route("/stockmarket")
-def stockmarket():
-    return render_template("stockmarket.html")
-
-
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
@@ -76,7 +71,7 @@ def toolbox():
     return render_template("toolbox.html")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(
         os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
